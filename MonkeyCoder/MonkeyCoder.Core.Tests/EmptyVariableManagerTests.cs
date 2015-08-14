f08948e2dc50cb87c50d7b8aef3365b2fd71c7e0
Ryszard Dżegan <ryszarddzegan@wp.pl>
2015-08-14 16:27:43 +0200

    Implement empty variable manager #16

---
+++ MonkeyCoder/MonkeyCoder.Core.Tests/EmptyVariableManagerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace MonkeyCoder.Core.Tests
+{
+    [TestClass]
+    public class EmptyVariableManagerTests
+    {
+        [TestMethod]
+        public void Enters_into_loop_once()
+        {
+            var vm = new EmptyVariableManager();
+
+            var loopCount = 0;
+            foreach (var vb in vm)
+                loopCount++;
+
+            AreEqual(1, loopCount);
+        }
+    }
+}
---
+++ MonkeyCoder/MonkeyCoder.Core/EmptyVariableManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MonkeyCoder.Core
+{
+    internal class EmptyVariableManager
+    {
+        public IEnumerator<dynamic> GetEnumerator()
+        {
+            yield return new VariableBag(new Dictionary<string, ValuePlaceholder>());
+        }
+    }
+}
