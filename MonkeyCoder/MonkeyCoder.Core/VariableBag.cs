59111707f0c5e752afb8ef5cce0bbdcd5d4f82fe
Ryszard Dżegan <ryszarddzegan@wp.pl>
2015-08-14 11:03:35 +0200

    Update xml comments of VariableBag class #14

--- MonkeyCoder/MonkeyCoder.Core/VariableBag.cs
+++ MonkeyCoder/MonkeyCoder.Core/VariableBag.cs
@@ -3,23 +3,38 @@ using System.Dynamic;
 
 namespace MonkeyCoder.Core
 {
+    /// <summary>
+    /// Name taken from <see href="https://msdn.microsoft.com/en-us/library/system.web.mvc.controllerbase.viewbag(v=vs.118).aspx">ControlerBase.ViewBag</see> of ASP.NET MVC.
+    /// Allows to access variables via dot notation as if they were properties of dynamic type.
+    /// </summary>
+    /// <remarks>
+    /// Parameters and variables called <c>vb</c> are of type <see cref="VariableBag"/>.
+    /// Using dictionaries wouldn't give us more type safety.
+    /// </remarks>
     public class VariableBag : DynamicObject
     {
-        public IDictionary<string, ValuePlaceholder> DecomposedVariables { get; }
+        /// <summary>
+        /// Pairs of variable name and associated value.
+        /// </summary>
+        public IDictionary<string, ValuePlaceholder> VariableValuePairs { get; }
 
-        public VariableBag(IDictionary<string, ValuePlaceholder> decomposedVariables)
+        /// <summary>
+        /// Constructor that takes pairs of variable name and associated value.
+        /// </summary>
+        /// <param name="variableValuePairs">Pairs of variable name and associated value.</param>
+        public VariableBag(IDictionary<string, ValuePlaceholder> variableValuePairs)
         {
-            DecomposedVariables = decomposedVariables;
+            VariableValuePairs = variableValuePairs;
         }
         
         /// <summary>Tries to return value for given variable name.</summary>
-        /// <param name="binder">Contains name that is provided to dynamic type.</param>
-        /// <param name="result">Value of the variable.</param>
+        /// <param name="binder">Contains variable name that is being used as property of given dynamic type.</param>
+        /// <param name="result">Value associated with the variable.</param>
         /// <returns>True if variable with provided name exists. False otherwise.</returns>
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
             ValuePlaceholder valuePlaceholder;
-            var ret = DecomposedVariables.TryGetValue(binder.Name, out valuePlaceholder);
+            var ret = VariableValuePairs.TryGetValue(binder.Name, out valuePlaceholder);
             result = valuePlaceholder.Value;
             return ret;
         }
