21ef165976d8c199a9b00999a4c8872b6691d572
Ryszard Dżegan <ryszarddzegan@wp.pl>
2015-08-18 14:46:00 +0200

    Implement variations without repetitions #37

---
+++ MonkeyCoder/MonkeyCoder.Core.Tests/Math/VariationsWithoutRepetitionsTests.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonkeyCoder.Core.Math;
+using System;
+using System.Linq;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.StringAssert;
+
+namespace MonkeyCoder.Core.Tests.Math
+{
+    using static MathTestsBase.StaticExpectedOutputReader;
+
+    [TestClass]
+    public class VariationsWithoutRepetitionsTests : MathTestsBase
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Throws_exception_when_pass_null_to_constructor()
+        {
+            new VariationsWithoutRepetitions<string>(null, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Throws_exception_when_k_is_less_than_zero()
+        {
+            try
+            {
+                new VariationsWithoutRepetitions<string>(Enumerable.Empty<string>(), -1);
+            }
+            catch (Exception exception)
+            {
+                StartsWith(exception.Message, "k must be greater or equal 0, but is -1.");
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Throws_exception_when_k_is_greater_than_items_size()
+        {
+            try
+            {
+                new VariationsWithoutRepetitions<string>(new[] { "a" }, 2);
+            }
+            catch (Exception exception)
+            {
+                StartsWith(exception.Message, "k must be less or equal items count, but is 2.");
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void Works_with_no_items_and_k_0()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(Enumerable.Empty<string>(), 0);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_1_item_and_k_0()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a" }, 0);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_1_item_and_k_1()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a" }, 1);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_2_items_and_k_0()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b" }, 0);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_2_items_and_k_1()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b" }, 1);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_2_items_and_k_2()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b" }, 2);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_3_items_and_k_0()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b", "c" }, 0);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_3_items_and_k_1()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b", "c" }, 1);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_3_items_and_k_2()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b", "c" }, 2);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_3_items_and_k_3()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b", "c" }, 3);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_4_items_and_k_0()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b", "c", "d" }, 0);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_4_items_and_k_1()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b", "c", "d" }, 1);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_4_items_and_k_2()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b", "c", "d" }, 2);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_4_items_and_k_3()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b", "c", "d" }, 3);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+
+        [TestMethod]
+        public void Works_with_4_items_and_k_4()
+        {
+            var variations = new VariationsWithoutRepetitions<string>(new[] { "a", "b", "c", "d" }, 4);
+            GenerateOutput(variations);
+            AreEqual(GetExpectedTestOutput(), GetActualTestOutput());
+        }
+    }
+}
---
+++ MonkeyCoder/MonkeyCoder.Core/Math/VariationsWithoutRepetitions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MonkeyCoder.Core.Math
+{
+    internal class VariationsWithoutRepetitions<T> : IEnumerable<IList<T>>
+    {
+        public IList<T> Items { get; }
+        public int K { get; }
+        
+        public VariationsWithoutRepetitions(IEnumerable<T> items, int k)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+
+            if (k < 0)
+                throw new ArgumentOutOfRangeException(nameof(k), $"k must be greater or equal 0, but is {k}.");
+
+            Items = items.ToList();
+
+            if (k > Items.Count)
+                throw new ArgumentOutOfRangeException(nameof(k), $"k must be less or equal items count, but is {k}.");
+
+            K = k;
+        }
+
+        private bool MoveNext(IList<int> bijection, IList<bool> freeItems)
+        {
+            for (var i = 0; i < bijection.Count(); i++)
+            {
+                var nextFreeItem = freeItems
+                    .Select((value, index) => new { FreeItemValue = value, FreeItemIndex = index })
+                    .Skip(bijection[bijection.Count - 1 - i])
+                    .FirstOrDefault(x => x.FreeItemValue);
+
+                freeItems[bijection[bijection.Count - 1 - i]] = true;
+
+                if (nextFreeItem == null)
+                    continue;
+                
+                bijection[bijection.Count - 1 - i] = nextFreeItem.FreeItemIndex;
+                freeItems[nextFreeItem.FreeItemIndex] = false;
+
+                var freeItemsToUpdate = freeItems
+                    .Select((value, index) => new { FreeItemValue = value, FreeItemIndex = index })
+                    .Where(x => x.FreeItemValue)
+                    .Select(x => x.FreeItemIndex)
+                    .Take(i)
+                    .Select((x, k) => new { FreeItemIndex = x, BijectionIndex = bijection.Count - i + k })
+                    .ToList();
+                
+                foreach (var freeItem in freeItemsToUpdate)
+                {
+                    bijection[freeItem.BijectionIndex] = freeItem.FreeItemIndex;
+                    freeItems[freeItem.FreeItemIndex] = false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<IList<T>> GetEnumerator()
+        {
+            if (K == 0 || !Items.Any())
+                yield break;
+
+            var bijection = Enumerable.Range(0, K).ToList();
+
+            var freeItems = Enumerable.Repeat(true, Items.Count).ToList();
+            for (var i = 0; i < K; i++)
+                freeItems[i] = false;
+
+            var resultQuery = from x in bijection
+                              select Items[x];
+
+            do yield return resultQuery.ToList();
+            while (MoveNext(bijection, freeItems));
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
