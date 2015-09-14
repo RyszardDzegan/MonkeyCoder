bf0c935a033bb6a9654337a4f360531477f82d92
Ryszard Dżegan <ryszarddzegan@wp.pl>
2015-09-14 10:57:21 +0200

    Implement public function manager factory #57

---
+++ MonkeyCoder/MonkeyCoder.Functions.Tests/FunctionManagerFactoryTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+namespace MonkeyCoder.Functions.Tests
+{
+    [TestFixture]
+    public class FunctionManagerFactoryTests
+    {
+        [Test]
+        public void Returns_function_manager()
+        {
+            var items = new object[] { 1, 2, 3 };
+            var tree = items.AsFunctionsTree();
+            IsInstanceOf<FunctionManager>(tree);
+        }
+    }
+}
---
+++ MonkeyCoder/MonkeyCoder.Functions/FunctionManagerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MonkeyCoder.Functions
+{
+    public static class FunctionManagerFactory
+    {
+        public static IEnumerable<Func<object>> AsFunctionsTree(this IEnumerable<object> items, int stackSize = 0)
+        {
+            if (items is IReadOnlyCollection<object>)
+                return new FunctionManager((IReadOnlyCollection<object>) items, stackSize);
+
+            if (items is IList<object>)
+                return new FunctionManager(new ReadOnlyCollection<object>((IList<object>) items), stackSize);
+
+            return new FunctionManager(items.ToList(), stackSize);
+        }
+    }
+}
