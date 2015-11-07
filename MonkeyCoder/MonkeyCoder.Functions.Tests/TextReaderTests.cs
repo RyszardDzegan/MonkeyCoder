e7957ab5267eecb2134af6bf4557206ca23a9e42
Ryszard Dżegan <ryszarddzegan@wp.pl>
2015-11-07 19:31:52 +0100

    Implement a basic call stack dumper. #74

---
+++ MonkeyCoder/MonkeyCoder.Functions.Tests/TextReaderTests.cs
@@ -0,0 +1,112 @@
+﻿using MonkeyCoder.Functions.Helpers.Arguments;
+using MonkeyCoder.Functions.Helpers.Invocations;
+using MonkeyCoder.Functions.Readers;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Linq;
+using static NUnit.Framework.Assert;
+
+namespace MonkeyCoder.Functions.Tests
+{
+    [TestFixture]
+    public class TextReaderTests
+    {
+        [Test]
+        public void Works_with_simple_value()
+        {
+            var items = new[] { 1 };
+            var invocations = items.AsFunctionsTree();
+            var e = invocations.GetEnumerator();
+            var sut = new TextInvocationReader();
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("1", sut.ToString());
+            IsFalse(e.MoveNext());
+        }
+
+        [Test]
+        public void Works_with_parameterless_delegate()
+        {
+            var func = new Func<string>(() => "a");
+            var items = new[] { func };
+            var invocations = items.AsFunctionsTree();
+            var e = invocations.GetEnumerator();
+            var sut = new TextInvocationReader();
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("foo()", sut.ToString());
+            IsFalse(e.MoveNext());
+        }
+
+        [Test]
+        public void Works_with_delegate()
+        {
+            var func = new Func<int, string>(x => "a" + x);
+            var items = new object[] { 1, func, 2 };
+            var invocations = items.AsFunctionsTree();
+            var e = invocations.GetEnumerator();
+            var sut = new TextInvocationReader();
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("1", sut.ToString());
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("foo(1)", sut.ToString());
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("foo(2)", sut.ToString());
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("2", sut.ToString());
+            IsFalse(e.MoveNext());
+        }
+
+        [Test]
+        public void Works_with_call_stack()
+        {
+            var func = new Func<string, string>(x => "a" + x);
+            var items = new object[] { func, "b" };
+            var invocations = items.AsFunctionsTree(1);
+            var e = invocations.GetEnumerator();
+            var sut = new TextInvocationReader();
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("foo(foo(\"b\"))", sut.ToString());
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("foo(\"b\")", sut.ToString());
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("\"b\"", sut.ToString());
+            IsFalse(e.MoveNext());
+        }
+
+        [Test]
+        public void Works_with_call_stack_and_two_functions()
+        {
+            var func1 = new Func<string, string>(x => "a" + x);
+            var func2 = new Func<string>(() => "b");
+            var items = new object[] { func1, func2, "c" };
+            var invocations = items.AsFunctionsTree(1);
+            var e = invocations.GetEnumerator();
+            var sut = new TextInvocationReader();
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("foo(foo(\"c\"))", sut.ToString());
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("foo(bar())", sut.ToString());
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("foo(\"c\")", sut.ToString());
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("bar()", sut.ToString());
+            IsTrue(e.MoveNext());
+            sut.Visit(e.Current);
+            AreEqual("\"c\"", sut.ToString());
+            IsFalse(e.MoveNext());
+        }
+    }
+}
---
+++ MonkeyCoder/MonkeyCoder.Functions/Readers/TextInvocationReader.cs
@@ -0,0 +1,92 @@
+﻿using MonkeyCoder.Functions.Helpers.Arguments;
+using MonkeyCoder.Functions.Helpers.Invocations;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MonkeyCoder.Functions.Readers
+{
+    /// <summary>
+    /// Formats informations regarding the information tree into a simple line of text.
+    /// </summary>
+    public class TextInvocationReader : IInvocationReader
+    {
+        private StringWriter TextWriter { get; set; }
+
+        private Queue<string> StandardFunctionNames { get; } =
+            new Queue<string>(new[] { "foo", "bar", "alpha", "beta", "delta", "gamma" });
+
+        private Dictionary<object, string> KnownFunctions { get; } =
+            new Dictionary<object, string>();
+
+        private int FunctionNameIndex = -1;
+
+        public void Visit(IInvocation invocation)
+        {
+            TextWriter = new StringWriter();
+            InternalVisit(invocation);
+        }
+
+        private void InternalVisit(IInvocation invocation)
+        {
+            if (invocation is DelegateInvocation)
+                Visit((DelegateInvocation)invocation);
+            else if (invocation is ValueInvocation)
+                Visit((ValueInvocation)invocation);
+        }
+
+        private void Visit(DelegateInvocation invocation)
+        {
+            TextWriter.Write(FormatValue(invocation.Delegate) + "(");
+
+            var lastArgument = invocation.Arguments.LastOrDefault();
+            foreach (var argument in invocation.Arguments)
+            {
+                if (argument is FunctionEvaluable)
+                    InternalVisit(((FunctionEvaluable)argument).Invocation);
+                else if (argument is ParameterlessArgument)
+                    TextWriter.Write(FormatValue(((ParameterlessArgument)argument).Value) + "()");
+                else
+                    TextWriter.Write(FormatValue(((IEvaluable)argument).Evaluate()));
+
+                if (argument != lastArgument)
+                    TextWriter.Write(",");
+            }
+
+            TextWriter.Write(")");
+        }
+
+        private void Visit(ValueInvocation invocation)
+        {
+            if (invocation.Value is string)
+                TextWriter.Write("\"" + invocation.Value + "\"");
+            else if (invocation.Value is char)
+                TextWriter.Write("'" + invocation.Value + "'");
+            else
+                TextWriter.Write(invocation.Value);
+        }
+
+        private string FormatValue(object value)
+        {
+            if (value is string)
+                return "\"" + value + "\"";
+
+            if (value is char)
+                return "'" + value + "'";
+
+            if (value is Delegate)
+            {
+                if (!KnownFunctions.ContainsKey(value))
+                    if (StandardFunctionNames.Any())
+                        KnownFunctions.Add(value, StandardFunctionNames.Any() ? StandardFunctionNames.Dequeue() : "f" + ++FunctionNameIndex);
+                return KnownFunctions[value];
+            }
+
+            return Convert.ToString(value);
+        }
+
+        public override string ToString() =>
+            TextWriter?.GetStringBuilder().ToString();
+    }
+}
